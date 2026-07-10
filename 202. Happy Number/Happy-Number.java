e2b0b3e41265ac0af6dd653abc3e300f0cf8fad5
umangbaliyan <umangbaliyan1e5@gmail.com>
2026-07-10 17:47:48 +0530

    add solution

---
+++ 202. Happy Number/Happy-Number.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+class Solution {
+    public boolean isHappy(int n) {
+         HashSet<Integer> set = new HashSet<>();
+        while(n!=1 && !set.contains(n)){
+        set.add(n);
+             int sum = 0;
+        while(n>0){
+            int digit = n%10;
+            sum = sum +digit*digit;
+            n = n/10;
+
+        }
+        n = sum;
+        }
+        return n == 1;
+    }
+}
