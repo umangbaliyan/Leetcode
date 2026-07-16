78022ebaeac4179c6849645ae8372d9e2ac1f9de
umangbaliyan <umangbaliyan1e5@gmail.com>
2026-07-16 22:46:35 +0530

    add solution

---
+++ 1572. Matrix Diagonal Sum/Matrix-Diagonal-Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+          int n = mat.length;
+        int sum = 0;
+        for (int i = 0; i<n; i++){
+            sum = sum + mat[i][i];
+            sum = sum + mat[i][n-1-i];
+        }
+        if(n%2==1){
+            sum = sum - mat[n/2][n/2];
+
+        }
+        return sum;
+    }
+}
---
+++ 169. Majority Element/Majority-Element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =0;
+int element=0;
+for(int i = 0; i<nums.length; i++){
+    if(count==0){
+        count = 1;
+        element = nums[i];
+    }
+    else if(nums[i]== element){
+        count++;
+    }
+    else{
+        count--;
+    }
+}
+return element;
+    }
+}
