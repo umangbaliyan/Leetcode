23e78e0086dc0fbee2f44e1ffa1d6f5d8599d970
umangbaliyan <umangbaliyan1e5@gmail.com>
2026-08-07 22:02:19 +0530

    add solution

---
+++ 49. Group Anagrams/Group-Anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+    
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+    
